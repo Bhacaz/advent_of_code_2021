a89f632289a730c4edc81d88d93d7653db40da4b
Jean-Francis Bastien <bhacaz@gmail.com>
2021-12-01 12:47:12 -0500

    day01 done

---
+++ day01/part1.rb
@@ -0,0 +1,12 @@
+class Part < BasePart
+  DAY = 1
+  def run
+    increase = 0
+    input.each_with_index do |deep, index|
+      next unless input[index - 1]
+
+      increase += 1 if deep > input[index - 1]
+    end
+    increase # => 1298
+  end
+end
